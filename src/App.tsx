07189520c431f05412ac3afc335ed1ada6feca17
waldisson <waldisson@hotmail.com>
2022-08-17 18:36:54 -0300

    feat: add navigation and styled components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () => {
+ 
+  return (
+    <View>
+      <Text>Appaaaaaaa</Text>
+    </View> 
+  )
+};
+
+export default App;
