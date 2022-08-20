105bd09e80fadd5aa040da825888697eb695fa4c
DevBethBit <106452219+DevBethBit@users.noreply.github.com>
2022-08-20 17:39:07 -0300

    chore: correctly in router

--- src/routes/drawer.routes.tsx
+++ src/routes/drawer.routes.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import {CustomDrawer} from '../components'
+import CustomDrawer from '../components/CustomDrawer'
 
 import HomeScreen from '../screens/Home';
+import colors from '../styles';
 
-const Drawer = createDrawerNavigator();
+type RootDrawerParams = {
+  Drawer: undefined;
+  Home: {name: string};
+  // Sector: {name: string};
+}
+
+const Drawer = createDrawerNavigator<RootDrawerParams>();
 
 function Router() {
   return (
     <Drawer.Navigator
       initialRouteName="Home"
       drawerContent={props => <CustomDrawer {...props} />}
+      screenOptions={{
+        drawerPosition: 'right',
+        headerTintColor: `${colors.white}`,
+        headerStyle:{
+          backgroundColor:`${colors.primaryMain}`,
+        }
+      }}
     >
       <Drawer.Screen 
         name="Home" 
