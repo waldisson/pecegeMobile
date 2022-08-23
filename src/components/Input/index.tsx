c53489a82312f33bad05c307fe8eb55350cabc9e
DevBethBit <106452219+DevBethBit@users.noreply.github.com>
2022-08-23 11:17:43 -0300

    chore: adjust in component input and add items

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,59 +1,225 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {TextInputProps} from 'react-native';
+import ArrowDownIcon from '../../assets/icons/arrowDown';
 import colors from '../../styles';
-
-// utils
-import {maskCep, maskPhone, maskCurrency} from '../../utils/mask';
+import { Item, ItemSector } from '../../types/Item';
 
 // Styles
 import {
   Container,
   Label,
   InputContent,
+  SelectContainer,
+  SelectButton,
+  SelectText,
+  SelectContent,
+  SelectItem,
+  SelectItemText,
+  SelectColorText,
 } from './styles';
 
-// Icons
+// colors select
+export const colorSelect = [
+  {
+    id: 1,
+    color: '#006FFD',
+  },
+  {
+    id: 2,
+    color: '#FCCA31',
+  },
+  {
+    id: 3,
+    color: '#F37F3A',
+  },
+  {
+    id: 4,
+    color: '#E02D3C',
+  }
+] 
+const LevelPriority = [
+  {
+    id:1,
+    priority: 'Alta prioridade'
+  },
+  {
+    id:2,
+    priority: 'Média prioridade'
+  },{
+    id:3,
+    priority: 'Baixa prioridade'
+  }
+]
+const LevelStatus = [
+  {
+    id:1,
+    status: 'Concluida'
+  },
+  {
+    id:2,
+    status: 'Pendente'
+  },{
+    id:3,
+    status: 'Em andamento'
+  }
+]
+
 
 // Props
 interface InputProps extends TextInputProps {
   mask?: 'cep' | 'currency' | 'phone';
+  variation: 'none' | 'sector' | 'status' | 'priority' | 'color';
   label: string;
   placeholder?: string;
+  setSectorSelect?: any;
+  sectorSelect?: any;
+  sectorList?:ItemSector[];
+  sectors?: any;
   message?: string;
   ref?: React.RefObject<any>;
   inputMaskChange?: any;
 }
 
 const Input: React.FC<InputProps> = props => {
-  const {label, placeholder} = props;
+  const {label, placeholder,sectorList ,sectors, sectorSelect,setSectorSelect, variation} = props;
 
-  // const handleChange = (text: string) => {
-  //   if(mask === 'cep') {
-  //     const value = maskCep(text);
-  //     inputMaskChange(value);
-  //   } 
-  //   if (mask === 'phone') {
-  //     const value = maskPhone(text);
-  //     inputMaskChange(value);
-  //   } 
-  //   if (mask === 'currency') {
-  //     const value = maskCurrency(text);
-  //     inputMaskChange(value);
-  //   } 
-
-  // }
+  const [openSector, setOpenSector] = useState(false);
 
   return (
     <Container 
       label={label}
+      variation={variation}
     >
-      <Label>{label}</Label>
-        <InputContent
-          placeholder={placeholder}
-          placeholderTextColor={colors.gray300}
-          // onChangeText={(text) => handleChange(text)}
-        {...props} 
-      />
+      
+      {
+        variation === 'none' &&
+        <>
+          <Label>{label}</Label>
+          <InputContent
+            placeholder={placeholder}
+            underlineColorAndroid="transparent"
+            placeholderTextColor={colors.gray300}
+            {...props} 
+          />
+        </>
+      }
+      {
+        variation === 'sector' &&
+        <>
+          <SelectContainer>
+            <Label>{label}</Label>
+            <SelectButton onPress={()=> setOpenSector(!openSector)}>
+                  <SelectText>{sectorSelect?.nameSector || placeholder}</SelectText>
+              <ArrowDownIcon />
+              
+            </SelectButton>
+            {
+              openSector && (
+                <SelectContent>
+                  {
+                    sectorList?.map((item)=>(
+                      <SelectItem key={item.id} onPress={()=>{
+                        setSectorSelect(item);
+                        setOpenSector(false);
+                      }}>
+                        <SelectItemText>{item.nameSector }</SelectItemText>
+                      </SelectItem>
+                    ))
+                  }
+                
+              </SelectContent>
+              )
+            }
+          </SelectContainer>
+        </>
+      }
+      {
+        variation === 'status' &&
+        <>
+          <SelectContainer>
+            <Label>{label}</Label>
+            <SelectButton onPress={()=> setOpenSector(!openSector)}>
+              <SelectText>{sectorSelect || placeholder}</SelectText>
+              <ArrowDownIcon />
+            </SelectButton>
+            {
+              openSector && (
+                <SelectContent>
+                  {
+                    LevelStatus?.map((item)=>(
+                      <SelectItem key={item.id} onPress={()=>{
+                        setSectorSelect(item.status);
+                        setOpenSector(false);
+                      }}>
+                        <SelectColorText>{item.status}</SelectColorText>
+                      </SelectItem>
+                    ))
+                  }
+                
+              </SelectContent>
+              )
+            }
+          </SelectContainer>
+        </>
+      }
+      {
+        variation === 'priority' &&
+        <>
+          <SelectContainer>
+            <Label>{label}</Label>
+            <SelectButton onPress={()=> setOpenSector(!openSector)}>
+              <SelectText>{sectorSelect || placeholder}</SelectText>
+              <ArrowDownIcon />
+            </SelectButton>
+            {
+              openSector && (
+                <SelectContent>
+                  {
+                    LevelPriority?.map((item)=>(
+                      <SelectItem key={item.id} onPress={()=>{
+                        setSectorSelect(item.priority);
+                        setOpenSector(false);
+                      }}>
+                        <SelectColorText>{item.priority}</SelectColorText>
+                        
+                      </SelectItem>
+                    ))
+                  }
+                
+              </SelectContent>
+              )
+            }
+          </SelectContainer>
+        </>
+      }
+      {
+        variation === 'color' &&
+        <>
+          <SelectContainer>
+            <SelectButton onPress={()=> setOpenSector(!openSector)}>
+              <SelectText>{sectorSelect || placeholder}</SelectText>
+              <ArrowDownIcon />
+            </SelectButton>
+            {
+              openSector && (
+                <SelectContent>
+                  {
+                    colorSelect?.map((item)=>(
+                      <SelectItem key={item.id} onPress={()=>{
+                        setSectorSelect(item.color);
+                        setOpenSector(false);
+                      }}>
+                        <SelectColorText>{item.color}</SelectColorText>
+                      </SelectItem>
+                    ))
+                  }
+                
+              </SelectContent>
+              )
+            }
+          </SelectContainer>
+        </>
+      }
     
   </Container>
   );
--- src/components/Input/styles.tsx
+++ src/components/Input/styles.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import colors from '../../styles';
+import {TouchableOpacity} from 'react-native'
 
 export const Container = styled.View`
   margin-bottom:16px;
@@ -17,4 +18,46 @@ export const InputContent = styled.TextInput`
   border: 1px solid ${colors.gray200};
   padding: 8px;
   height:34px;
+  width:100%;
+`;
+export const SelectContainer = styled.View`
+  position: relative;
+  
+`;
+
+export const SelectButton = styled.TouchableOpacity`
+  border: 1px solid ${colors.gray200};
+  padding:0 8px 0 8px;
+  align-items:center;
+  justify-content: space-between;
+  height:34px;
+  width:100%;
+  border-radius:8px;
+  flex-direction: row;
+  z-index:-1;
+`;
+
+export const SelectText = styled.Text`
+  
+`;
+export const SelectContent = styled.ScrollView`
+  position:absolute;
+  bottom:35px;
+  z-index:999;
+  width:100%;
+  padding: 8px;
+  border-radius: 4px;
+  background-color:${colors.primary};
+`;
+export const SelectItem = styled(TouchableOpacity).attrs({
+  activeOpacity:0.9
+})`
+  padding:5px 0px;
+  background-color:${colors.white};
+  margin-top:5px;
+  padding-left: 5px;
+`;
+export const SelectItemText = styled.Text`
+`;
+export const SelectColorText = styled.Text`
 `;
