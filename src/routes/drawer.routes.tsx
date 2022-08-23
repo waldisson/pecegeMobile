4bcc849fab7d25f8db82cba6ac843863057c8f59
DevBethBit <106452219+DevBethBit@users.noreply.github.com>
2022-08-23 11:20:52 -0300

    chore: organization code

--- src/routes/drawer.routes.tsx
+++ src/routes/drawer.routes.tsx
@@ -1,72 +1,90 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import CustomDrawer from '../components/CustomDrawer';
+import {TouchableOpacity} from 'react-native';
+import {
+  CommonActions,
+  DrawerActions,
+  useNavigation,
+} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import colors from '../styles';
 
 // pages
-import HomeScreen from '../screens/Home';
+import TasksScreen from '../screens/Task';
 import SectorsScreen from '../screens/Sectors';
+
+// components
+import CustomDrawer from '../components/CustomDrawer';
 
 // icons
 import MenuIcon from '../assets/icons/menu';
-import { DrawerActions, useNavigation } from '@react-navigation/native';
-
+import ArrowLeftCircleIcon from '../assets/icons/arrowLeftCircle';
 
 // type
 export type RootDrawerParams = {
   Drawer: undefined;
   Home: {name: string};
+  Task: {name: string};
   Sector: {name: string};
-}
+};
 
 const Drawer = createDrawerNavigator<RootDrawerParams>();
 
 function Router() {
   const navigation = useNavigation();
+
+  const goTo = (name: string, params?: any) => {
+    navigation.dispatch({
+      ...CommonActions.navigate({name, params}),
+    });
+  };
+
   return (
     <Drawer.Navigator
-      initialRouteName="Home"
+      initialRouteName="Task"
       drawerContent={props => <CustomDrawer {...props} />}
       screenOptions={{
         drawerPosition: 'right',
         headerTintColor: `${colors.white}`,
-        headerStyle:{
-          backgroundColor:`${colors.primaryMain}`,
-        }
-      }}
-    >
-      <Drawer.Screen 
-        name="Home" 
-        component={HomeScreen}
+        headerStyle: {
+          backgroundColor: `${colors.primaryMain}`,
+        },
+      }}>
+      <Drawer.Screen
+        name="Sector"
+        component={SectorsScreen}
+        options={{
+          headerTitle: 'Setores',
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{marginLeft: 20}}
+              onPress={() => goTo('Task')}>
+              <ArrowLeftCircleIcon />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              style={{marginRight: 20}}
+              onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+              <MenuIcon />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Task"
+        component={TasksScreen}
         options={{
           headerTitle: 'Meu quadro',
           headerLeft: false,
           headerRight: () => (
-            <TouchableOpacity style={{marginRight: 20}} onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+            <TouchableOpacity
+              style={{marginRight: 20}}
+              onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
               <MenuIcon />
             </TouchableOpacity>
           ),
         }}
       />
-      <Drawer.Screen 
-        name="Sector" 
-        component={SectorsScreen} 
-        options={{
-          headerTitle: 'Meu quadro',
-          headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.goBack()}>
-              <MenuIcon />
-            </TouchableOpacity>
-          ),
-          headerRight: () => (
-            <TouchableOpacity style={{marginRight: 20}} onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
-              <MenuIcon />
-            </TouchableOpacity>
-          ),
-        }}
-      />
-      
     </Drawer.Navigator>
   );
 }
